d1924eacc5683514ad73bfdffa1f03423fb3e0d5
Darjush <darjush@Darjushs-MacBook-Pro.local>
2017-09-28 17:44:19 +0200

    Aktuelle Version

--- ebztabelle.php
+++ ebztabelle.php
@@ -10,7 +10,7 @@
 
 <body>
   <div class="btn-group btn-group-justified">
-    <a href="ebztabelledownload.php" class="btn btn-primary">Tabelle Herunterladen</a>
+    <a href="ebztabelledownload.php" target="_blank" class="btn btn-primary">Tabelle Herunterladen</a>
     <a href="ebz.php" class="btn btn-primary">EBZ Kursbezeichnung Erfassen</a>
   </div></br></br>
   <div class="table-responsive">
@@ -56,18 +56,20 @@
        if (preg_match('/^G/', $gebauderow) == "1") {
            $gebaude = "GIBS";
            return $gebaude;
-       }
-       else{
-         $gebaude = "KBS";
-         return $gebaude;
+       } else {
+           $gebaude = "KBS";
+           return $gebaude;
        }
    }
 
-   function getstock($gebauderow){
 
-
-
+   function getFloor($gebauderow)
+   {
+       $search = array("K01","K03","K003","K16","K18","K21","K101","K103","K108","K112","K114","K201","K203","K208","K210","K212","K213","K214","K215","K301","K303","K304","K308","K309","K311","K313","K315","K316","CBBZ","Aula KBS","GE01","GE02","GE06","GE07","GE10","GE11","GE14","GE15","GE18","GE20","GE21","GE22","GE23","GE24","GE25","GE26","GE27","GE28","GE29","GE30/32","GE62","G053","G102","G104","G105","G106","G107","G109","G111","G113","G151","G154","G155","G157","G158","G159","G202","G204","G205","G207","G209","G211","G212","G214","G302","G304","G305","G310","G308","G310","G311","G314L","G402","G404","G405","G407","G409","G411","G412","G413","G501","G503","Aula GIBS","Foyer GIBS","TH1","TH2","TH3","TH4");
+       $replace = array("UG","UG","EG","EG","EG","EG","1","1","1","1","1","2","2","2","2","2","2","2","2","3","3","3","3","3","3","3","3","3","1","1","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","EG","1","1","1","1","1","1","1","1","1","1","1","1","1","1","2","2","2","2","2","2","2","2","3","3","3","3","3","3","3","3","4","4","4","4","4","4","4","4","5","5","EG","EG","1","1","1","1");
+       return str_replace($search, $replace, $gebauderow);
    }
+
 
    //______________________________________________Raumverwaltung________________________________________________________
    //Variabeln
@@ -99,7 +101,7 @@
            $bemerkung = bemerkungsfilter($bemerkungrow);
            $gebaude = gebaudefinder($gebauderow);
            echo "<tr><td>" . "" . "</td><td>" . $oldrow[2] . "</td><td>" . "Platzhalter Tag" . "</td><td>" . $oldrow[3]. "</td><td>" . $oldrow[4]. "</td><td>" . $oldrow[6] . "</td><td>" . "Platzhalter Referent" .
-     "</td><td>" . $gebaude . "</td><td>" . $oldrow[1] ."</td><td>"."</td><td>"."</td><td>".$bemerkung."</td><td>"."</td><td>"."</td><td>"."</td><td>"."Ja"."</td></tr>";  //$row['index'] the index here is a field name
+     "</td><td>" . $gebaude . "</td><td>" . $oldrow[1] ."</td><td>".$stock."</td><td>"."</td><td>".$bemerkung."</td><td>"."</td><td>"."</td><td>"."</td><td>"."Ja"."</td></tr>";  //$row['index'] the index here is a field name
            $written = true;
            $oldrow = $newrow;
        }
@@ -109,8 +111,9 @@
        $bemerkungrow = $newrow[6];
        $bemerkung = bemerkungsfilter($bemerkungrow);
        $gebaude = gebaudefinder($gebauderow);
+       $stock = getFloor($gebauderow);
        echo "<tr><td>" . ""  . "</td><td>" . $newrow[2] . "</td><td>" . "Platzhalter Tag" . "</td><td>" . $newrow[3]. "</td><td>" . $newrow[4]. "</td><td>" . $newrow[6] . "</td><td>" . "Platzhalter Referent" .
-    "</td><td>" . $gebaude  . "</td><td>" . $newrow[1] ."</td><td>"."</td><td>"."</td><td>".$bemerkung."</td><td>"."</td><td>"."</td><td>"."</td><td>"."Ja"."</td></tr>";  //$row['index'] the index here is a field name
+    "</td><td>" . $gebaude  . "</td><td>" . $newrow[1] ."</td><td>".$stock."</td><td>"."</td><td>".$bemerkung."</td><td>"."</td><td>"."</td><td>"."</td><td>"."Ja"."</td></tr>";  //$row['index'] the index here is a field name
    }
 
 //______________________________________________StundenPlan________________________________________________________
@@ -140,8 +143,9 @@ $newrow = "";
            $teacher = $row[16];
            $gebauderow = $oldrow[0];
            $gebaude = gebaudefinder($gebauderow);
+           $stock = getFloor($gebauderow);
            echo "<tr><td>" . "  " . "</td><td>" . $oldrow[2] . "</td><td>" . "Platzhalter Tag" . "</td><td>" . $oldrow[3]. "</td><td>" . $oldrow[4]. "</td><td>" . $oldrow[14] . "</td><td>" . $oldrow[16] .
-      "</td><td>" . $gebaude . "</td><td>" . $oldrow[0] . "</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."Ja"."</td></tr>";  //$row['index'] the index here is a field name  //$row['index'] the index here is a field name
+      "</td><td>" . $gebaude . "</td><td>" . $oldrow[0] . "</td><td>".$stock."</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."Ja"."</td></tr>";  //$row['index'] the index here is a field name  //$row['index'] the index here is a field name
            $written = true;
            $oldrow = $row;
            $oldrow = $newrow;
@@ -150,8 +154,9 @@ $newrow = "";
 if ($written == false) {
     $gebauderow = $newrow[0];
     $gebaude = gebaudefinder($gebauderow);
+    $stock = getFloor($gebauderow);
     echo "<tr><td>" . "  " . "</td><td>" . $newrow[2] . "</td><td>" . "Platzhalter Tag" . "</td><td>" . $newrow[3]. "</td><td>" . $newrow[4]. "</td><td>" . $newrow[14] . "</td><td>" . $newrow[16] .
-"</td><td>" . $gebaude . "</td><td>" . $newrow[0] . "</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."Ja"."</td></tr>";  //$row['index'] the index here is a field name  //$row['index'] the index here is a field name
+"</td><td>" . $gebaude . "</td><td>" . $newrow[0] . "</td><td>".$stock."</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."</td><td>"."Ja"."</td></tr>";  //$row['index'] the index here is a field name  //$row['index'] the index here is a field name
 }
 
 
@@ -165,3 +170,4 @@ if ($written == false) {
     </table>
   </div>
 </body>
+</html>
