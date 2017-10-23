30859bbff3ee19d59646683aa5654eed2ec31bad
Unknown <mr.a.j.qureshi@gmail.com>
2017-10-23 16:23:51 +0200

    navbar color changed

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   <script src="js/jquery.js"></script>
   <script src="js/bootstrap.js"></script>
 
-  <nav class="navbar navbar-inverse">
+  <nav class="navbar navbar-inverse" style="background-color:#8cc63f;">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -14,16 +14,16 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">BBZO Planungen</a>
+      <a class="navbar-brand" href="#" style="color:black;">BBZO Planungen</a>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="buchungen.php">Buchungen</a></li>
-        <li><a href="ebztabelle.php">EBZ</a></li>
-        <li><a href="wp.php">Stundenplanung</a></li>
-        <li><a href="rv.php">Raumverwaltung</a></li>
-        <li><a href="about.php">About</a></li>
+    <div class="collapse navbar-collapse" id="myNavbar" >
+      <ul class="nav navbar-nav" >
+        <li><a href="index.php" style="color:black;">Home</a></li>
+        <li><a href="buchungen.php" style="color:black;">Buchungen</a></li>
+        <li><a href="ebztabelle.php" style="color:black;">EBZ</a></li>
+        <li><a href="wp.php" style="color:black;">Stundenplanung</a></li>
+        <li><a href="rv.php" style="color:black;">Raumverwaltung</a></li>
+        <li><a href="about.php" style="color:black;">About</a></li>
       </ul>
     </div>
   </div>
